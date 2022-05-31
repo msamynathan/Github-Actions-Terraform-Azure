65bae8e8bcc0aadbfe8c37792ab98704595a8020
samy <samynathan.muthirulappan@gmail.com>
2022-05-31 08:00:34 -0400

    update

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ provider   "azurerm"   {
    location                =   "northeurope" 
    resource_group_name     =   azurerm_resource_group.rg.name 
    network_interface_ids   =   [ azurerm_network_interface.myvm1nic.id ] 
-   size                    =   "Standard_B1s" 
+   size                    =   "Standard_B1ms" 
    admin_username          =   "adminuser" 
    admin_password          =   "Password123!" 
 
